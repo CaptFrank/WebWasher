e2c67078bbadbe8844a2f0a2e1a5180012e079c9
Francis <francispapineau@gmail.com>
2016-03-10 22:13:47 -0500

    Finished mqtt and wifi

--- node/Debug/objects.mk
+++ node/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -Wl,-T"C:/Program\ Files/energia-0101E0015/hardware/cc3200/cores/cc3200/cc3200.ld" -l"lpcc3200_MQTTClient" -l"lpcc3200_WiFi" -l"lpcc3200_core" -l"lpcc3200_Wire" -l"m" -l"c" -l"gcc"
+LIBS := -Wl,-T"C:/Users/Francis\ Papineau/Documents/energia-0101E0017/hardware/cc3200/cores/cc3200/cc3200.ld" -l"lpcc3200_MQTTClient" -l"lpcc3200_WiFi" -l"lpcc3200_core" -l"lpcc3200_Wire" -l"m" -l"c" -l"gcc"
 
--- node/Debug/subdir_rules.mk
+++ node/Debug/subdir_rules.mk
@@ -13,7 +13,7 @@ node.cpp: ../node.ino
 %.o: ./%.cpp $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU Compiler'
-	"C:/Program Files/energia-0101E0015/hardware/tools/lm4f/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -fno-exceptions -DF_CPU=80000000L -DARDUINO=101 -DENERGIA=13 -I"C:/Program Files/energia-0101E0015/hardware/cc3200/cores/cc3200" -I"C:/Users/francis-ccs/git/WebWasher/node" -I"C:/Program Files/energia-0101E0015/hardware/cc3200/variants/launchpad" -I"C:/Program Files/energia-0101E0015/hardware/cc3200/libraries/WiFi" -I"C:/Program Files/energia-0101E0015/hardware/cc3200/libraries/MQTTClient" -I"C:/Program Files/energia-0101E0015/hardware/cc3200/libraries/Wire" -Os -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -fno-rtti -o"$@" $(GEN_OPTS__FLAG) "$<"
+	"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/tools/lm4f/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -fno-exceptions -DF_CPU=80000000L -DARDUINO=101 -DENERGIA=13 -I"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/cc3200/cores/cc3200" -I"C:/Users/francis-ccs/git/WebWasher/node" -I"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/cc3200/variants/launchpad" -I"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/cc3200/libraries/WiFi" -I"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/cc3200/libraries/MQTTClient" -I"C:/Users/Francis Papineau/Documents/energia-0101E0017/hardware/cc3200/libraries/Wire" -Os -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -fno-rtti -o"$@" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
